10e0f43f08d959abff57343ab27f32c303654366
MarcHarnist <31386370+MarcHarnist@users.noreply.github.com>
2019-07-15 15:21:00 +0200

    add xml file

---
+++ JavaEE/src/fr/marcharnist/javaee/PremierServletConfigurationXml.java
@@ -0,0 +1,38 @@
+package fr.marcharnist.javaee;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PremierServletConfigurationXml
+ */
+public class PremierServletConfigurationXml extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public PremierServletConfigurationXml() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Configuration par web.xml - Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
